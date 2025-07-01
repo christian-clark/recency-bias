19e2bad3e9446f051943c312dc96c4973592a7a2
Christian Clark <clark.3664@osu.edu>
2025-07-01 17:47:55 -0400

    add files for transformers repo

---
+++ transformers/src/transformers/models/gpt_neox_alibi/modeling_gpt_neox_alibi.py
@@ -0,0 +1,184 @@
+# coding=utf-8
+# Copyright 2022 EleutherAI The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" PyTorch GPTNeoX model using attention with linear biases
+(ALiBi, Press et al 2023) instead of rotary embeddings."""
+
+import torch
+import torch.utils.checkpoint
+from torch import nn
+from ..gpt_neox.modeling_gpt_neox import (
+    GPTNeoXAttention,
+    GPTNeoXForCausalLM,
+    GPTNeoXLayer,
+    GPTNeoXMLP,
+    GPTNeoXModel
+)
+
+class GPTNeoXAlibiAttention(GPTNeoXAttention):
+    def __init__(self, config):
+        super(GPTNeoXAttention, self).__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_size = self.hidden_size // self.num_attention_heads
+        max_positions = config.max_position_embeddings
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones((max_positions, max_positions), dtype=torch.uint8)).view(
+                1, 1, max_positions, max_positions
+            ),
+        )
+        self.register_buffer("masked_bias", torch.tensor(-1e9))
+        self.norm_factor = torch.sqrt(torch.tensor(self.head_size, dtype=torch.float32)).to(torch.get_default_dtype())
+        self.query_key_value = nn.Linear(config.hidden_size, 3 * config.hidden_size)
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        # NOTE the zeros in the buffer are placeholders for ALiBi slopes that
+        # are added to the model by gpt-neox/tools/convert_sequential_to_hf_alibi.py
+        # to add alibi slopes at inference time instead of training time, comment this out
+        self.register_buffer(
+            "alibi_slopes",
+            torch.zeros(self.num_attention_heads)
+        )
+
+    def forward(
+        self,
+        hidden_states,
+        attention_mask,
+        head_mask=None,
+        layer_past=None,
+        use_cache=False,
+        output_attentions=False,
+    ):
+        has_layer_past = layer_past is not None
+
+        # Compute QKV
+        # Attention heads [batch, seq_len, hidden_size]
+        #   --> [batch, seq_len, (np * 3 * head_size)]
+        qkv = self.query_key_value(hidden_states)
+
+        # [batch, seq_len, (num_heads * 3 * head_size)]
+        #   --> [batch, seq_len, num_heads, 3 * head_size]
+        new_qkv_shape = qkv.size()[:-1] + (self.num_attention_heads, 3 * self.head_size)
+        qkv = qkv.view(*new_qkv_shape)
+
+        # [batch, seq_len, num_attention_heads, 3 * head_size] --> 3 [batch, num_attention_heads, seq_len, head_size]
+        query = qkv[..., : self.head_size].permute(0, 2, 1, 3)
+        key = qkv[..., self.head_size : 2 * self.head_size].permute(0, 2, 1, 3)
+        value = qkv[..., 2 * self.head_size :].permute(0, 2, 1, 3)
+
+        # Cache QKV values
+        if has_layer_past:
+            past_key = layer_past[0]
+            past_value = layer_past[1]
+            key = torch.cat((past_key, key), dim=-2)
+            value = torch.cat((past_value, value), dim=-2)
+        present = None if use_cache else (key, value)
+
+        # Compute attention
+        attn_output, attn_weights = self._attn(query, key, value, attention_mask, head_mask)
+
+        # Reshape outputs
+        attn_output = self._merge_heads(attn_output, self.num_attention_heads, self.head_size)
+        attn_output = self.dense(attn_output)
+
+        outputs = (attn_output, present)
+        if output_attentions:
+            outputs += (attn_weights,)
+
+        return outputs
+
+    def _attn(self, query, key, value, attention_mask=None, head_mask=None):
+        # q, k, v: [bs, num_attention_heads, seq_len, attn_head_size]
+        # compute causal mask from causal mask buffer
+        batch_size, num_attention_heads, query_length, attn_head_size = query.size()
+        key_length = key.size(-2)
+
+        causal_mask = self.bias[:, :, key_length - query_length : key_length, :key_length].bool()
+
+        query = query.view(batch_size * num_attention_heads, query_length, attn_head_size)
+        key = key.view(batch_size * num_attention_heads, key_length, attn_head_size)
+        attn_scores = torch.einsum("bik,bjk->bij", query, key) / self.norm_factor
+        attn_scores = attn_scores.view(batch_size, num_attention_heads, query_length, key_length)
+
+        # add ALiBi matrix
+        # based on this code:
+        #https://github.com/ofirpress/attention_with_linear_biases/blob/master/fairseq/models/transformer.py#L742
+        
+        # NOTE to add alibi slopes at inference time instead of training time, comment this out
+        # [num_attention_heads, 1]
+        slopes = self.alibi_slopes.unsqueeze(-1)
+
+        # NOTE uncomment this to add alibi slopes at inference time instead of training time
+        # standard ALiBi
+        #slopes = torch.tensor([1/4, 1/16, 1/64, 1/256]).unsqueeze(-1)
+        #slopes = torch.tensor([1.]*num_attention_heads).unsqueeze(-1)
+        #slopes = torch.tensor([1/2]*num_attention_heads).unsqueeze(-1)
+        #slopes = torch.tensor([1/16]*num_attention_heads).unsqueeze(-1)
+        #slopes = torch.tensor([1/256]*num_attention_heads).unsqueeze(-1)
+        # [1, key_len]
+        positions = torch.arange(key_length).unsqueeze(0).float()
+        # [1, num_attention_heads, 1, key_len]
+        alibi = torch.matmul(slopes, positions).unsqueeze(0).unsqueeze(-2)
+        # [bs, num_attention_heads, query_len, key_len]
+        alibi = alibi.expand(batch_size, -1, query_length, -1)
+        attn_scores = attn_scores + alibi
+
+        attn_scores = torch.where(causal_mask, attn_scores, self.masked_bias.to(attn_scores.dtype))
+
+        if attention_mask is not None:
+            # Apply the attention mask
+            attn_scores = attn_scores + attention_mask
+
+        attn_weights = nn.functional.softmax(attn_scores, dim=-1)
+        attn_weights = attn_weights.to(value.dtype)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attn_weights = attn_weights * head_mask
+
+        attn_output = torch.matmul(attn_weights, value)
+        return attn_output, attn_weights
+
+
+class GPTNeoXAlibiLayer(GPTNeoXLayer):
+    def __init__(self, config):
+        super(GPTNeoXLayer, self).__init__()
+        self.input_layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.post_attention_layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.attention = GPTNeoXAlibiAttention(config)
+        self.mlp = GPTNeoXMLP(config)
+
+
+class GPTNeoXAlibiModel(GPTNeoXModel):
+    def __init__(self, config):
+        super(GPTNeoXModel, self).__init__(config)
+        self.config = config
+
+        self.embed_in = nn.Embedding(config.vocab_size, config.hidden_size)
+        self.layers = nn.ModuleList([GPTNeoXAlibiLayer(config) for _ in range(config.num_hidden_layers)])
+        self.final_layer_norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+
+
+class GPTNeoXAlibiForCausalLM(GPTNeoXForCausalLM):
+    def __init__(self, config):
+        super(GPTNeoXForCausalLM, self).__init__(config)
+        self.gpt_neox = GPTNeoXAlibiModel(config)
+        self.embed_out = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+
+        # Initialize weights and apply final processing
+        self.post_init()
